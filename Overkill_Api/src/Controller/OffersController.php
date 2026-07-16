23947f79fa2326db9c8c2ccbc471a07c5b6fa73d
Razigue Ben Hmida <razigue.benhmida@epitech.eu>
2026-07-16 16:43:19 +0200

    add: a DTO for offers data, and the beginning of our POST route, still wip

--- Overkill_Api/src/Controller/OffersController.php
+++ Overkill_Api/src/Controller/OffersController.php
@@ -2,20 +2,53 @@
 
 namespace App\Controller;
 
+use App\Dto\OfferInput;
 use App\Repository\OffersRepository;
+use App\Repository\SourcesRepository;
+use App\Repository\CategoriesRepository;
+use App\Repository\CompaniesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpFoundation\Response;
 
 final class OffersController extends AbstractController
 {
     #[Route('/api/offers', name: 'api_offers', methods: ['GET'])]
     public function getOffer(OffersRepository $offers): JsonResponse
     {
-        $existingoffers = $offers->findAll();
-        return $this->json($existingoffers, 200, [], ['groups' => 'offers:read']);
+        $existingOffers = $offers->findAll();
+        return $this->json($existingOffers, 200, [], ['groups' => 'offers:read']);
     }
-}
+    
+    #[Route('/api/offers', name: 'api_create_offers', methods: ['POST'])]
+    public function setOffer(
+        #[MapRequestPayload]
+        OfferInput $input,
+        CompaniesRepository $companies,
+        CategoriesRepository $categories,
+        SourcesRepository $sources,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
 
-  
+
+    
+        $company = $companies->find($input->company_id);
+
+        if ($company == null) {
+            return $this->json("L'id reçu ne correspond à rien",   Response::HTTP_NOT_FOUND);
+        }
+        
+        $source = $sources->find($input->source_id);
+
+        if ($source == null) {
+            return $this->json("La source reçu ne correspond à rien",   Response::HTTP_NOT_FOUND);
+        }
+
+
+    }, Response::HTTP_CREATED;
+
+}
